dfce9aaea2643ea1bae123f95e36bb2ba2ff0ffe
Leandro Scardua <leandroscar@gmail.com>
2020-08-20 15:05:30 -0400

    Update install-control.sh

--- controler-node/ubuntu/18.04/install-control.sh
+++ controler-node/ubuntu/18.04/install-control.sh
@@ -2,3 +2,5 @@ sudo apt update
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
+apt install python-pip
+pip install pywinrm
