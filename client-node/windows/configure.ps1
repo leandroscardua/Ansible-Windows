941204df2e696768d7452825514b6544ec00100a
Leandro Scardua <leandroscar@gmail.com>
2021-01-12 14:02:34 -0500

    Create configure.ps1

---
+++ client-node/windows/configure.ps1
@@ -0,0 +1,5 @@
+$wc = New-Object Net.WebClient
+
+$wc.DownloadString('https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1') > \ConfigureRemotingForAnsible.ps1
+
+\ConfigureRemotingForAnsible.ps1 -ForceNewSSLCert true
