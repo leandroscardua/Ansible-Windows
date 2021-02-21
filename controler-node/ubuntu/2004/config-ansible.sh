f7e9ef238c3b8758ff8609378deabe54c4887a3f
Leandro Scardua <leandroscar@gmail.com>
2021-02-21 15:58:36 -0500

    Update config-ansible.sh

--- controler-node/ubuntu/2004/config-ansible.sh
+++ controler-node/ubuntu/2004/config-ansible.sh
@@ -1,6 +1,6 @@
 folder=ansible
 
-echo 'Configure folder tree'
+echo 'Configuring folder tree'
 
 cd ~/
 mkdir $folder
@@ -9,9 +9,13 @@ mkdir log
 mkdir roles
 mkdir playbooks
 
-echo 'Change the ansible configuration'
+echo 'Changing the ansible configuration'
 
 cp /etc/ansible/ansible.cfg ~/$folder/ansible.cfg
 echo "export ANSIBLE_CONFIG=~/$folder/ansible.cfg" >> ~/.bashrc
 sed -i 's+#log_path = /var/log/ansible.log+log_path = ~/'$folder'/log/ansible.log+g' ansible.cfg
 sed -i 's+#roles_path    = /etc/ansible/roles+roles_path = ~/'$folder'/roles+g' ansible.cfg
+
+echo 'Reloading terminal'
+
+exec bash
