e412e91eb80ca01c48b0266c52954a6bac97b97b
Leandro Scardua <leandroscar@gmail.com>
2021-03-17 21:34:04 -0400

    Update configure.ps1

--- client-node/windows/configure.ps1
+++ client-node/windows/configure.ps1
@@ -1,5 +1,6 @@
-$wc = New-Object Net.WebClient
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "\ConfigureRemotingForAnsible.ps1"
 
-$wc.DownloadString('https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1') > .\ConfigureRemotingForAnsible.ps1
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
 
-.\ConfigureRemotingForAnsible.ps1 -ForceNewSSLCert true
+powershell.exe -ExecutionPolicy ByPass -File $file
