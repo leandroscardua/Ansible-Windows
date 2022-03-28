960caf613e7480d38d1f7c8c745eeb06362fc922
Leandro Scardua <leandroscar@gmail.com>
2022-03-28 11:14:44 -0400

    Update config-ansible-auto.sh

--- controler-node/ubuntu/2004/config-ansible-auto.sh
+++ controler-node/ubuntu/2004/config-ansible-auto.sh
@@ -13,9 +13,11 @@ mkdir playbooks
 echo 'Changing the ansible configuration'
 
 cp /etc/ansible/ansible.cfg /home/$usera/$folder/ansible.cfg
-echo "export ANSIBLE_CONFIG=/home/$usera/$folder/ansible.cfg" >> ~/.bashrc
-sed -i 's+#log_path = /var/log/ansible.log+log_path = /home/$usera/$folder/log/ansible.log+g' ansible.cfg
-sed -i 's+#roles_path = /etc/ansible/roles+roles_path = /home/$usera/$folder/roles+g' ansible.cfg
+echo "export ANSIBLE_CONFIG=/home/'$usera/$folder'/ansible.cfg" >> ~/.bashrc
+sed -i 's+#log_path = /var/log/ansible.log+log_path = /home/'$usera/$folder'/log/ansible.log+g' ansible.cfg
+sed -i 's+#roles_path    = /etc/ansible/roles+roles_path = ~/'$usera/$folder'/roles+g' ansible.cfg
+
+chown -R ansible:ansible ./ansible/
 
 echo 'Reloading terminal'
 
